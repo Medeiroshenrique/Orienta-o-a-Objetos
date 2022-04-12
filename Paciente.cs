15a1b5090e327405602da2b969934af454669d16
Henrique Medeiros <henrique.c.r.medeiros@gmail.com>
2022-04-12 14:05:40 -0300

    orientacao a objetos

---
+++ Paciente.cs
@@ -0,0 +1,18 @@
+﻿class Paciente
+    {
+        private string _nome;
+        public sbyte Idade;
+        private string _cpf;
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
+        
+       
+    }
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paciente p1 =new Paciente();
+            p1.SetNome("Jhonatta");
+            p1.Idade =20;
+            Paciente p2 = new Paciente();
+            p2.SetNome("Daniel");
+            p2.Idade = 22;
+            Paciente p3 = new Paciente();
+            p3.SetNome("Marcela");
+            p3.Idade = 21;
+            Console.WriteLine(p1.GetNome());
+            Console.WriteLine(p1.Idade);
+            Console.WriteLine(p2.GetNome());
+            Console.WriteLine(p2.Idade);
+            Console.WriteLine(p3.GetNome());
+            Console.WriteLine(p3.Idade);
+
+        }  
+    }
+}
